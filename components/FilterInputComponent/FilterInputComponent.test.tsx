9ab77570770d57453d1ac2ddc0c42e62ef7e0240
Alexander Pagliaro <alex@limit-point.com>
2024-05-20 02:50:15 -0400

    Add unit test

--- components/FilterInputComponent/FilterInputComponent.test.tsx
+++ components/FilterInputComponent/FilterInputComponent.test.tsx
@@ -1,3 +1,29 @@
+import { FilterInputComponent } from "@/components/FilterInputComponent/FilterInputComponent";
+import { useExampleData } from "@/lib/Data/useExampleData";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 describe("FilterInputComponent", () => {
-  it.todo("should add tokens");
+  it("should add a token with the inputted filter attributes", async () => {
+    const dataSource = useExampleData();
+    const mockOnAddFilter = jest.fn();
+    render(
+      <FilterInputComponent
+        filters={[]}
+        dataSource={dataSource}
+        onAddFilter={mockOnAddFilter}
+        onDeleteFilter={jest.fn()}
+      />
+    );
+    await userEvent.click(screen.getByRole("textbox"));
+    await userEvent.click(screen.getByText("Genre"));
+    await userEvent.click(screen.getByText("="));
+    await userEvent.click(screen.getByText("Deep House"));
+    expect(mockOnAddFilter).toBeCalledWith(
+      expect.objectContaining({
+        operator: "=",
+        property: expect.objectContaining({ name: "Genre" }),
+        value: expect.objectContaining({ value: "Deep House" }),
+      })
+    );
+  });
 });
